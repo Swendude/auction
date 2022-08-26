19aeb593aae71d9641056f47a6eb007c6553cb17
Swen Mulderij <swenmulderij@gmail.com>
2022-08-26 10:06:39 +0200

    Small fix

--- src/store/thunks.ts
+++ src/store/thunks.ts
@@ -1,10 +1,11 @@
+import { Action, ThunkDispatch } from "@reduxjs/toolkit";
 import axios from "axios";
 import { RootState, AppDispatch } from ".";
 import { setPokemon } from "./counter/slice";
 
 export const getPokemon =
   (pokemonName: string) =>
-  async (dispatch: AppDispatch, getState: RootState) => {
+  async (dispatch: AppDispatch, getState: () => RootState) => {
     try {
       const response = await axios.get(
         `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
